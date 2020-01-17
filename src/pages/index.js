b54852286578405da8dd675d089f54caa75b119f
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-16 22:59:26 -0500

    Banner
    add banner.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import links from "../constants/links"
 
 export default () => (
   <Layout>
-    <SimpleHero />
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="nunc consequat interdum varius sit amet mattis vulputate enim nulla"
+      >
+        <Link to="tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
