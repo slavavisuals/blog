97ad4d46fa5faae2b7a750f6f224eb1206176fec
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-26 18:05:46 -0500

    updated blog
    updated blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const blog = () => {
+const blog = ({ data }) => {
   return (
     <Layout>
       hello from blog page
       <Link to="/">back home</Link>
+      <h1>title: {data.site.siteMetadata.title}</h1>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          age
+        }
+      }
+    }
+  }
+`
 
 export default blog
