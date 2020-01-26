4c8fbb3a1f1b0ab312105eba66022a4da1a748e1
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-25 19:08:18 -0500

    about section
    about section added

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,11 +1,34 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
 const About = () => {
   return (
-    <div>
-      Hello from about!!
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            quis ante nunc. Donec vulputate diam leo, at pretium sapien dapibus
+            ut.
+          </p>
+          <p>
+            Nullam sed porttitor felis, a aliquet purus. Sed eget porta lacus.
+            Donec vulputate diam leo, at pretium sapien dapibus ut.
+          </p>
+          <button type="button" className="btn-primary">
+            readmore
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,18 +1,17 @@
 import React from "react"
 import styled from "styled-components"
-
-const Title = ({ title, subtitle }) => {
+const Title = ({ title, subtitle, className }) => {
   return (
-    <TitleWrapper>
+    <div className={className}>
       <h4>
         <span className="title">{title}</span>
         <span>{subtitle}</span>
       </h4>
-    </TitleWrapper>
+    </div>
   )
 }
 
-const TitleWrapper = styled.div`
+export default styled(Title)`
   text-transform: uppercase;
   font-size: 2.3rem;
   margin-bottom: 2rem;
@@ -34,5 +33,3 @@ const TitleWrapper = styled.div`
     }
   }
 `
-
-export default Title
