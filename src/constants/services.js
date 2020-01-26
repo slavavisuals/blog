48d13a013fa21d37d30ad6548fcdf5830a12a713
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-25 20:16:09 -0500

    added services
    added services

---
+++ src/constants/services.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text: "lsdfdsf sdfdsf sdfdsf sdfsdf sdfsdfs, dsfjfjfjfjf, dfdfdfdf",
+  },
+  {
+    icon: <FaTree />,
+    title: "endless hiking",
+    text: "lsdfdsf sdfdsf sdfdsf sdfsdf sdfsdfs, dsfjfjfjfjf, dfdfdfdf",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text: "lsdfdsf sdfdsf sdfdsf sdfsdf sdfsdfs, dsfjfjfjfjf, dfdfdfdf",
+  },
+]
