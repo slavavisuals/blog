f47fa03ef840315a9d92678a893621b88017879a
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-26 17:53:19 -0500

    useStaticQuery
    useStaticQuery was added

--- src/examples/RegularHeader.js
+++ src/examples/RegularHeader.js
@@ -1,11 +1,35 @@
-import react from "react"
-
-const Component = () => {
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+const getSiteData = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          age
+        }
+      }
+    }
+  }
+`
+const RegularHeader = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <StaticQuery
+      query={getSiteData}
+      render={data => {
+        console.log(data)
+
+        return (
+          <div>
+            <h1> title : {data.site.siteMetadata.title}</h1>
+            <h4> title : {data.site.siteMetadata.author}</h4>
+          </div>
+        )
+      }}
+    />
   )
 }
 
-export default Component
+export default RegularHeader
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
-import Button from "../examples/Button"
-import Header from "../examples/Header"
+import Header from "../examples/RegularHeader"
 export default class tours extends Component {
   render() {
     return (
