910e82f4f1d0c3c6a82179c03024695e1a7dee51
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-01-28 23:02:57 -0500

    About
    add about image using gatsby image

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 const blog = () => {
   return (
     <Layout>
       hello from blog page
       <Link to="/">back home</Link>
-      <Images />
     </Layout>
   )
 }
