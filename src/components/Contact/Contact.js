693b6de3925027e0c167bfbd8ed7b3692ddf95e1
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-02-04 13:52:22 -0500

    form
    add form

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Title from "../Title"
+import styles from "../../css/contact.module.css"
+
+const Contact = () => {
+  return (
+    <section className={styles.contact}>
+      <Title title="contact" subtitle="us" />
+      <div className={styles.center}>
+        <form className={styles.form}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className={styles.formControl}
+              placeholder="john smith"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className={styles.formControl}
+              placeholder="john@smith.com"
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Message</label>
+            <textarea
+              name="message"
+              rows="10"
+              className={styles.formControl}
+              placeholder="Hello there"
+            />
+          </div>
+          <div>
+            <input type="submit" value="send" className={styles.submit} />
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
