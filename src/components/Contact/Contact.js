16f46be2bdf3e884bf5490226cf40a8954ba8bd6
slavavisuals <53318274+slavavisuals@users.noreply.github.com>
2020-02-04 14:53:14 -0500

    adding variables
    adding variables

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,11 @@ const Contact = () => {
     <section className={styles.contact}>
       <Title title="contact" subtitle="us" />
       <div className={styles.center}>
-        <form className={styles.form}>
+        <form
+          action="https://formspree.io/example@gmail.com"
+          method="POST"
+          className={styles.form}
+        >
           <div>
             <label htmlFor="name">Name</label>
             <input
