f350f09110f85bf9568e1150f235ef5e639411c1
slavavisuals <stambovtsev@canadagoose.com>
2020-02-18 18:58:54 -0500

    fixing other commits conflicts

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import BlogCard from './BlogCard'
 import Title from '../Title'
@@ -15,25 +14,6 @@ query{
           slug
           image{
             fluid{
-=======
-import React from "react"
-import BlogCard from "./BlogCard"
-import Title from "../Title"
-import { useStaticQuery, graphql } from "gatsby"
-import styles from "../../css/blog.module.css"
-
-const getPosts = graphql`
-  query {
-    posts: allContentfulBlogpost(sort: { fields: published, order: DESC }) {
-      edges {
-        node {
-          published(formatString: "MMMM Do, YYYY")
-          title
-          slug
-          id: contentful_id
-          image {
-            fluid {
->>>>>>> f1272d2d0a85a29b5a71b90c478e065637296369
               ...GatsbyContentfulFluid
             }
           }
@@ -44,7 +24,6 @@ const getPosts = graphql`
 `
 
 const BlogList = () => {
-<<<<<<< HEAD
     const {blogposts} = useStaticQuery(getBlogPosts)
 console.log(blogposts);
     return (
@@ -60,21 +39,3 @@ console.log(blogposts);
 }
 
 export default BlogList
-=======
-  const { posts } = useStaticQuery(getPosts)
-  console.log(posts)
-
-  return (
-    <section className={styles.blog}>
-      <Title title="our" subtitle="blogs" />
-      <div className={styles.center}>
-        {posts.edges.map(({ node }) => {
-          return <BlogCard key={node.id} blog={node} />
-        })}
-      </div>
-    </section>
-  )
-}
-
-export default BlogList
->>>>>>> f1272d2d0a85a29b5a71b90c478e065637296369
